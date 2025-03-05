5acefbb48010022ccea94cda58144f9dedd853ca
HarikrishnanHere <itsmeharikrishnanv@gmail.com>
2025-03-05 18:49:01 +0530

    Started Backend. COmpleted Login and Signup APIs

---
+++ backend/controllers/userController.ts
@@ -0,0 +1,88 @@
+import { Request, RequestHandler, Response } from "express";
+import userModel from "../models/userModel";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
+
+const createToken = (id: ObjectId) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+//Route for user Login
+const loginUser: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.json({ success: false, message: "User Doesn't Exist" });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+//Route for User Registration
+const registerUser: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { name, email, password } = req.body;
+
+    //Checking if user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      res.json({ success: false, message: "User Already Exists" });
+      return;
+    }
+
+    //validating email format and strong password
+    if (!validator.isEmail(email)) {
+      res.json({ success: false, message: "Please enter a valid Email" });
+      return;
+    }
+    if (password.length < 8) {
+      res.json({ success: false, message: "Please enter a strong password" });
+      return;
+    }
+
+    //Hashing User Password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+//Route for Admin Login
+const adminLogin: RequestHandler = async (req: Request, res: Response) => {};
+
+export { loginUser, registerUser, adminLogin };
---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.ts";
+import connectCloudinary from "./config/cloudinary.ts";
+import userRouter from "./routes/userRoute.ts";
+
+//App Config
+
+const app: Application = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+
+app.use(express.json());
+app.use(cors());
+
+//api end points
+app.use("/api/user", userRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log("Server started on PORT : " + port);
+});
